8f60ce23d4152aee948dbeaf9d503e99506f6514
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-10-03 14:01:15 +0700

    gtg

--- js/chart.js
+++ js/chart.js
@@ -3,18 +3,18 @@ const ctx = document.getElementById('myChart');
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Januari', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni','Juli','Agustus','September', 'Oktober'],
       datasets: [{
         label: 'Suhu Udara',
-        data: [37, 19, 3, 5, 2, 3],
+        data: [34.5, 34.3, 35.3, 34, 35,34.5, 34.3, 33.3, 34, 35],
         borderWidth: 1
       }]
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: false
-        }
-      }
-    }
+    // options: {
+    //   scales: {
+    //     y: {
+    //       beginAtZero: false
+    //     }
+    //   }
+    // }
   });
