18e9f1a664f1cbf2559c7701f6e2f7877dbfe213
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-10-03 14:32:50 +0700

    frf

--- js/chart.js
+++ js/chart.js
@@ -5,16 +5,79 @@ const ctx = document.getElementById('myChart');
     data: {
       labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni','Juli','Agustus','September', 'Oktober'],
       datasets: [{
-        label: 'Suhu Udara',
-        data: [34.5, 34.3, 35.3, 34, 35,34.5, 34.3, 33.3, 34, 35],
+        label: 'Celcius',
+        data: [34.5, 33.3, 34.3, 34, 35,34.5, 34.3, 33.3, 34, 35],
         borderWidth: 1
       }]
     },
-    // options: {
-    //   scales: {
-    //     y: {
-    //       beginAtZero: false
-    //     }
-    //   }
-    // }
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: "Rata-Rata Suhu Udara",
+          color: "black",
+          font: {
+            family: "'Montserrat'", // Your font family
+            size: 20,
+            weight: "bold",
+          },
+  
+          padding: {
+            top: 10,
+            bottom: 30,
+          },
+        },
+      },
+      scales: {
+        x: {
+          title: {
+            display: true,
+            color: "black",
+            text: "Bulan",
+            font: {
+              family: "'Montserrat'", // Your font family
+              size: 14,
+              weight: "bold",
+            },
+          },
+          ticks: {
+            color: "black",
+            font: {
+              family: "'Montserrat'", // Your font family
+              size: 14,
+            },
+          },
+        },
+        y: {
+          title: {
+            display: true,
+            color: "black",
+            text: "Derajat Celcius",
+            font: {
+              family: "'Montserrat'", // Your font family
+              size: 14,
+              weight: "bold",
+            },
+            padding: 20,
+          },
+          ticks: {
+            color: "black",
+            font: {
+              family: "'Montserrat'", // Your font family
+              size: 14,
+            },
+          },
+        },
+      },
+      animations: {
+        backgroundColor: {
+          duration: 2500,
+          easing: "linear",
+          to: "blue",
+          from: "#28B7FC",
+          type: "color",
+          loop: true,
+        },
+      },
+    },
   });
