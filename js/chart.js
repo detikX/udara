e32ac4bd73d6268871a69e24d00deacd0d248eba
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-10-04 08:27:14 +0700

    ded

--- js/chart.js
+++ js/chart.js
@@ -1,85 +1,87 @@
 const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni','Juli','Agustus','September', 'Oktober'],
-      datasets: [{
-        label: 'Celcius',
-        data: [34.5, 33.3, 34.3, 34, 35,34.5, 34.3, 33.3, 34, 35],
-        borderWidth: 0
-      }]
-    },
-    maintainAspectRatio: true,
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober'],
+    datasets: [{
+      label: 'Celcius',
+      data: [34.5, 33.3, 34.3, 34, 35, 34.5, 34.3, 33.3, 34, 35],
+      borderWidth: 0
+    }]
+  },
+  options: {
     responsive: true,
-    options: {
-      plugins: {
+    maintainAspectRatio: false,
+    aspectRatio: 1 / 2,
+    plugins: {
+      title: {
+        display: true,
+        text: "Rata-Rata Suhu Udara",
+        color: "black",
+        font: {
+          family: "'Montserrat'", // Your font family
+          size: 20,
+          weight: "bold",
+        },
+
+        padding: {
+          top: 10,
+          bottom: 30,
+        },
+      },
+    },
+    scales: {
+      x: {
         title: {
           display: true,
-          text: "Rata-Rata Suhu Udara",
+          color: "black",
+          text: "Bulan",
+          font: {
+            family: "'Montserrat'", // Your font family
+            size: 14,
+            weight: "bold",
+          },
+        },
+        ticks: {
           color: "black",
           font: {
             family: "'Montserrat'", // Your font family
-            size: 20,
+            size: 14,
+          },
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          color: "black",
+          text: "Derajat Celcius",
+          font: {
+            family: "'Montserrat'", // Your font family
+            size: 14,
             weight: "bold",
           },
-  
-          padding: {
-            top: 10,
-            bottom: 30,
-          },
+          padding: 20,
         },
-      },
-      scales: {
-        x: {
-          title: {
-            display: true,
-            color: "black",
-            text: "Bulan",
-            font: {
-              family: "'Montserrat'", // Your font family
-              size: 14,
-              weight: "bold",
-            },
+        ticks: {
+          color: "black",
+          beginAtZero: false,
+          font: {
+            family: "'Montserrat'", // Your font family
+            size: 14,
           },
-          ticks: {
-            color: "black",
-            font: {
-              family: "'Montserrat'", // Your font family
-             size: 14,
-            },
-          },
-        },
-        y: {
-          title: {
-            display: true,
-            color: "black",
-            text: "Derajat Celcius",
-            font: {
-              family: "'Montserrat'", // Your font family
-              size: 14,
-              weight: "bold",
-            },
-            padding: 20,
-          },
-          ticks: {
-            color: "black",
-            font: {
-              family: "'Montserrat'", // Your font family
-              size: 14,
-            },
-          },
-        },
-      },
-      animations: {
-        backgroundColor: {
-          duration: 2500,
-          easing: "linear",
-          to: "orange",
-          from: "#e37e42",
-          type: "color",
-          loop: true,
         },
       },
     },
-  });
+    animations: {
+      backgroundColor: {
+        duration: 2500,
+        easing: "linear",
+        to: "orange",
+        from: "#e37e42",
+        type: "color",
+        loop: true,
+      },
+    },
+  },
+});
